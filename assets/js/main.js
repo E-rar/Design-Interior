1aa055cb6318230d1968e4021682c7c920b4f756
erik-ramich <erik.ramich@gmx.de>
2021-07-27 14:30:04 +0200

    upload

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+function output(){
+    console.log(document.getElementById("Name").value);
+    console.log(document.getElementById("Email").value);
+    console.log(document.getElementById("Message").value);
+
+}
